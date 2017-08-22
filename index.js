15c3be1ded782a0335a321196621287a14f5127c
Spencer Henze <spencerhenze@Spencers-iMac.local>
2017-08-21 19:13:43 -0600

    should work...

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ var database = require("./config/dbconfig")
 var server = express();
 
 server.use(cors());
+var port = 3000;
 
 server.use(bodyParser.json());
 
 var blogRouter = require('./routes/blog-routes');
 server.use('/api/blogs', blogRouter);
+
+
+server.listen(port, () => {
+    console.log(`starting up Node, on port: `, port)
+})
